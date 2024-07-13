5d6a006d11d692b00b5880e760507e010b1b1e9d
Nomaan <nomaan07.dev@gmail.com>
2024-07-13 23:24:47 +0330

    refactor: toggle search box display

--- js/app.js
+++ js/app.js
@@ -1,11 +1,21 @@
 const overlay = document.querySelector('.overlay');
-const searchBoxToggleBtn = document.querySelector('.header__search-icon--1200');
-const searchBox = document.querySelector('.header__search-box--1200');
+const headerSearchBoxToggleBtn = document.querySelector('.header__search-icon--1200');
+const headerSearchBox = document.querySelector('.header__search-box--1200');
+const headerSearchInput1200 = document.querySelector('.header__search-input--1200');
 
-const toggleSearchBoxHandler = () => {
-  overlay.classList.toggle('hidden');
-  searchBox.classList.toggle('hidden');
+const addHiddenClassByOverlay = () => {
+  overlay.classList.add('hidden');
+  headerSearchBox.classList.add('hidden');
 };
 
-searchBoxToggleBtn.addEventListener('click', toggleSearchBoxHandler);
-overlay.addEventListener('click', toggleSearchBoxHandler);
+const showSearchBox = () => {
+  headerSearchInput1200.value = '';
+  setTimeout(() => {
+    headerSearchInput1200.focus();
+  }, 0);
+  overlay.classList.remove('hidden');
+  headerSearchBox.classList.remove('hidden');
+};
+
+headerSearchBoxToggleBtn.addEventListener('click', showSearchBox);
+overlay.addEventListener('click', addHiddenClassByOverlay);
